3ce6f2d7a5e4a946ef235984b971fe160ddd425e
Victor Häggqvist <victor@snilius.com>
2014-03-01 02:01:09 +0100

    nude-cli

--- nude-cli.js
+++ nude-cli.js
@@ -1,13 +1,40 @@
 var nude = require("nude");
-var image = process.argv[2];
+var first = process.argv[2];
+var second = process.argv[3];
 
-// No file, display help
-if (image == undefined){
-  console.log("Usage: nude-cli file");
+if (first == "-h" || first == "--help"){
+  console.log("Usage: nude-cli [options] file");
+  console.log("v0.0.1");
+  console.log("Options:");
+  process.stdout.write("-v --verbose Verbose output"+'\n'+
+                       "-i --integer Return 1 or 0 instead of a boolean"+'\n'+
+                       "-h --help Display help"+'\n');
   process.exit(1);
 }
 
+// No file, display help
+if (first === undefined){
+  console.log("Usage: nude-cli [options] file");
+  process.exit(1);
+}
+
+// first is image if no second
+var image = (second === undefined)?first:second;
+
 // Do the thing
-nude.scan(__dirname + "/" + process.argv[2], function(res) {
-    console.log('Contains nudity: ' + res);
+nude.scan(__dirname + "/" + image, function(res) {
+  switch (first){
+    case "-v":
+    case "--verbose":
+      process.stdout.write("File: "+image+'\n'+
+                           "Contains nudity: " + res+'\n');
+      break;
+    case "-i":
+    case "--integer":
+      console.log((res)?"1":"0");
+      break;
+    default:
+      console.log(res);
+      break;
+  }
 });
